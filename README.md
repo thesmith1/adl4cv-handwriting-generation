8a60a82936df871b3279d8137130945b8e2689ea
Giorgio Fabbro <appfan3@gmail.com>
2018-11-15 22:39:25 +0100

    Implemented the class cGAN and the base classes for ConditionalDiscriminator and ConditionalGenerator. Two classes inheriting them are added but not yet fully implemented.

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ Repository for Project in Advanced Deep Learning for Computer Vision at TUM
 
 ## Installation
 
-to create a new virtual environment with the required dependencies:
+To create a new virtual environment with the required dependencies:
 
 ```conda env create -f adl4cv-env.yml```
+
+## Structure
+The folder `model/` contains the GAN classes and the models of all discriminators and generators.
---
+++ model/componentsGAN.py
@@ -0,0 +1,32 @@
+import abc
+
+from torch.autograd import Variable
+from torch.nn import Module
+
+
+class ConditionalDiscriminator(Module, metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def __init__(self):
+        super().__init__()
+        raise NotImplementedError('Should have implemented this.')
+
+    def __call__(self, x: Variable, c: Variable):
+        self.forward(x, c)
+
+    @abc.abstractmethod
+    def forward(self, x: Variable, c: Variable):
+        raise NotImplementedError('Should have implemented this.')
+
+
+class ConditionalGenerator(Module, metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def __init__(self):
+        super().__init__()
+        raise NotImplementedError('Should have implemented this.')
+
+    def __call__(self, z: Variable, c: Variable):
+        self.forward(z, c)
+
+    @abc.abstractmethod
+    def forward(self, z: Variable, c: Variable):
+        raise NotImplementedError('Should have implemented this.')
