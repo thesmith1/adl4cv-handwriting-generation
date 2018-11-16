2a900314d78ad780d993e3516307d110fe157f21
Paolo Notaro <paulstpr@gmail.com>
2018-11-16 17:21:10 +0100

    added script to crop images with selection rectangle

---
+++ utils/crop_utility.py
@@ -0,0 +1,88 @@
+import cv2
+import numpy as np
+from sys import argv
+FATAL = -1
+
+win_name = "Crop image"
+rectangle_shape = (50, 50)
+white = (255, 255, 255)
+
+
+class Rectangle:
+
+    def __init__(self, p0, width, height):
+        self.x0, self.y0 = p0
+        self.w, self.h = width, height
+
+    def is_point_inside(self, point):
+
+        x, y = point
+        return self.x0 <= x <= (self.x0 + self.w) and self.y0 <= y <= (self.y0 + self.h)
+
+
+class RectangleDragHandler:
+
+    def __init__(self, window, rectangle):
+        self.window = window
+        self.rectangle = rectangle
+        self.dragging = False
+        self.previous_top_left_coordinates = (0, 0)
+        self.previous_coordinates = (0, 0)
+
+    def mouse_callback(self, event, x, y, flags=None, param=None):
+
+        if event == cv2.EVENT_LBUTTONDBLCLK:
+            if self.rectangle.is_point_inside(point=(x, y)):
+                print("Double click inside rectangle!")
+            else:
+                print("Double click!")
+        elif event == cv2.EVENT_LBUTTONDOWN:
+            if self.rectangle.is_point_inside(point=(x, y)):
+                self.dragging = True
+                self.previous_top_left_coordinates = self.rectangle.x0, self.rectangle.y0
+                self.previous_coordinates = x, y
+                print("Dragging")
+        elif event == cv2.EVENT_MOUSEMOVE:
+            if self.dragging:
+                self.rectangle.x0 = x - self.previous_coordinates[0] + self.previous_top_left_coordinates[0]
+                self.rectangle.y0 = y - self.previous_coordinates[1] + self.previous_top_left_coordinates[1]
+        elif event == cv2.EVENT_LBUTTONUP:
+            self.dragging = False
+
+            print("Stopped dragging")
+
+
+def main_program_window(image):
+    h, w, _ = img.shape
+
+    cv2.imshow(win_name, image)
+
+    h_rect, w_rect = rectangle_shape
+    x0_rect, y0_rect = (w - w_rect) // 2, (h - h_rect) // 2
+    rect = Rectangle(p0=(x0_rect, y0_rect), height=h_rect, width=w_rect)
+    handler = RectangleDragHandler(win_name, rect)
+    cv2.setMouseCallback(win_name, handler.mouse_callback)
+
+    done = False
+    while not done:
+
+        image_with_rect = np.copy(image)
+        cv2.rectangle(image_with_rect, pt1=(rect.x0, rect.y0), pt2=(rect.x0 + w_rect, rect.y0 + h_rect), color=white)
+        cv2.imshow(win_name, image_with_rect)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            done = True
+
+
+if __name__ == '__main__':
+    if len(argv) != 2:
+        print("Invalid input arguments. Usage: python crop_utility.py <image-path>")
+        exit(FATAL)
+
+    # open image
+    image_path = argv[1]
+    img = cv2.imread(image_path)
+    if img is None:
+        print("Invalid path to image specified. '%s' not found" % image_path)
+        exit(FATAL)
+
+    main_program_window(img)
