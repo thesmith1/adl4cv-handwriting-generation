bec21f746f5218e4f9f1cf514adcc1cdfef3c7af
Paolo Notaro <paulstpr@gmail.com>
2018-11-20 22:23:18 +0100

    improved preprocessing quality

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -7,34 +7,35 @@ base_dir = "../data"
 source_dir = os.path.join(base_dir, "raw")
 dest_dir = os.path.join(base_dir, "processed")
 
-WHITE_THRESHOLD = 120
 WHITE = 255
 BORDER_WIDTH = 5
 
 
 def process_image(image):
+
     # gray-scaling
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # median filter
-    image = cv2.medianBlur(image, 3)
-
     # 2-means clustering white saturation
-    mu = [0, 255]
-    z = np.zeros_like(image)
-    z_previous = None
-    while np.any(z != z_previous):
+    mu = [80, 160]
+    z = np.zeros_like(image, dtype=np.uint8)
+    z_previous = np.ones_like(image)
+    while not np.all(z == z_previous):
 
-        z_previous = z
+        z_previous = np.copy(z)
         # E-step
         for i, row in enumerate(image):
             for j, element in enumerate(row):
                 distances = [np.linalg.norm(element - mu_i) for mu_i in mu]
                 z[i, j] = np.argmin(distances)
         # M-step
-        mu = [np.mean(image[z == i]) for i in [0, 1]]
-
+        for i in (0, 1):
+            if np.sum(z == i) == 0:
+                mu[i] = 80 * (i + 1)
+            else:
+                mu[i] = np.mean(image[z == i])
     image[z == 1] = WHITE
+    print(mu)
 
     # border removal
     image[0:BORDER_WIDTH, :] = WHITE
@@ -50,10 +51,11 @@ def process_image(image):
 
 if __name__ == '__main__':
 
-    for filename in os.listdir(source_dir):
+    all_filenames = os.listdir(source_dir)
+    for file_count, filename in enumerate(all_filenames):
         if is_image(filename):
-            print("Processing '%s'..." % filename)
+            print("Processing '%s' [%3d/%3d]..." % (filename, file_count + 1, len(all_filenames)), end='\r')
             image_path = os.path.join(source_dir, filename)
-            image = cv2.imread(image_path)
-            processed_image = process_image(image)
+            img = cv2.imread(image_path)
+            processed_image = process_image(img)
             cv2.imwrite(os.path.join(dest_dir, filename), processed_image)
