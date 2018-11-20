f2e82c110355b810f641e55ea95dfb08efff7cd6
Giorgio Fabbro <ga53kug@mytum.de>
2018-11-20 17:41:08 +0100

    Implemented method in cGAN to visualize generated samples during training.

--- model/cGAN.py
+++ model/cGAN.py
@@ -1,4 +1,5 @@
 import torch
+from matplotlib.pyplot import imshow, show
 from numpy.random import randn
 from torch.nn.modules.loss import _Loss, BCELoss
 from torch.optim import Optimizer, Adam
@@ -29,8 +30,19 @@ class CGAN:
         self._G.to(device=self._device)
         self._D.to(device=self._device)
 
-    def train(self, n_epochs: int):
+    def generate(self, character: str, do_print: bool = False):
+        assert len(character) == 1
+        c = character_to_one_hot(character)
+        c = torch.from_numpy(c).to(device=device)
+        z = torch.from_numpy(randn(1, NOISE_LENGTH, 1)).to(device=self._device)
+        output = self._G(z, c)
+        if do_print:
+            imshow(output[0].cpu().detach().numpy().transpose(1, 2, 0).squeeze())
+            show()
+        return output
 
+    def train(self, n_epochs: int):
+        max_GPU_memory = 0
         # Iterate epochs
         print('Starting epochs, GPU memory in use '
               'before loading the inputs: {} MB'.format(torch.cuda.memory_allocated(torch.cuda.current_device())/1e6))
@@ -38,23 +50,22 @@ class CGAN:
             # Iterate the dataset
             D_loss, G_loss = None, None
             for X, c, style in self._dataset_loader:  # TODO: use style
-
                 # Reset gradient
                 self._D_optim.zero_grad()
 
                 # Sample data
-                z = torch.from_numpy(randn(len(X), NOISE_LENGTH, 1)).to(device=device)
-                X = X.to(device=device)
+                z = torch.from_numpy(randn(len(X), NOISE_LENGTH, 1)).to(device=self._device)
+                X = X.to(device=self._device)
                 c = character_to_one_hot(c)
-                c = torch.from_numpy(c).to(device=device)
+                c = torch.from_numpy(c).to(device=self._device)
 
                 # Discriminator forward-loss-backward-update
                 G_sample = self._G(z, c)
                 D_real = self._D(X, c)
                 D_fake = self._D(G_sample, c)
 
-                zero_label = torch.zeros(len(X), 1).to(device=device)
-                one_label = torch.ones(len(X), 1).to(device=device)
+                zero_label = torch.zeros(len(X), 1).to(device=self._device)
+                one_label = torch.ones(len(X), 1).to(device=self._device)
 
                 D_loss_real = self._D_loss(D_real, one_label)
                 D_loss_fake = self._D_loss(D_fake, zero_label)
@@ -63,11 +74,11 @@ class CGAN:
                 D_loss.backward()
                 self._D_optim.step(None)
 
-                # Generator forward-loss-backward-update
-
                 # Reset gradient
                 self._G_optim.zero_grad()
-                z = torch.from_numpy(randn(len(X), NOISE_LENGTH, 1)).to(device)
+
+                # Generator forward-loss-backward-update
+                z = torch.from_numpy(randn(len(X), NOISE_LENGTH, 1)).to(self._device)
 
                 G_sample = self._G(z, c)
                 D_fake = self._D(G_sample, c)
@@ -77,8 +88,13 @@ class CGAN:
                 G_loss.backward()
                 self._G_optim.step(None)
 
-            if epoch % 5 == 0:
+                # Store max allocated GPU memory
+                max_GPU_memory = max(max_GPU_memory, torch.cuda.max_memory_allocated(torch.cuda.current_device())/1e6)
+
+            if epoch % 50 == 0:
                 print('Epoch {}: D loss {}, G loss {}'.format(epoch, D_loss, G_loss))
+                print('Max GPU memory allocated: {} MB'.format(max_GPU_memory))
+                self.generate('A', True)
 
 
 if __name__ == '__main__':
@@ -92,7 +108,7 @@ if __name__ == '__main__':
     d = D1()
     g_adam = Adam(g.parameters())
     d_adam = Adam(d.parameters())
-    dataset = CharacterDataset('../data/processed/', '../data/labels_test.txt', Compose([Pad(7), ToTensor()]))
+    dataset = CharacterDataset('../data/img/', '../data/labels_test.txt', Compose([Pad(7), ToTensor()]))
     loader = DataLoader(dataset, batch_size=2, shuffle=True)
     gan = CGAN(g, d, BCELoss(), BCELoss(), g_adam, d_adam, loader, device)
-    gan.train(100)
+    gan.train(1000)
