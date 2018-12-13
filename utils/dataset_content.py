c67a790f0c36ccf3fbca45000f718c86d1fe7c4c
Giorgio Fabbro <ga53kug@mytum.de>
2018-12-13 16:45:37 +0100

    Corrected dataset content search. Still missing management of sequences of letters.

--- utils/dataset_content.py
+++ utils/dataset_content.py
@@ -1,21 +1,32 @@
+import pickle
+from datetime import datetime
+from os import listdir
+from os.path import isfile, join
+from random import sample, choice
+
 import numpy as np
+from matplotlib import pyplot as plt
 from numpy.random import choice as npchoice
 from numpy.random import randint
-from global_vars import NUM_CHARS, character_to_index_mapping
-from random import sample, shuffle, choice
-import pickle
-from matplotlib import pyplot as plt
+from pycrayon import CrayonClient
 
+from global_vars import NUM_CHARS, character_to_index_mapping
 
 # Evolutionary parameters
-target_distribution = np.ones((26,))/26
-solution_size = 100
-mutation_probability = 0.1
-crossover_probability = 0.75
-selection_percentage = 0.8
-population_size = 1000
+target_distribution = np.ones((70,))/70
+original_text_distribution = np.zeros((70,))/70
+solution_size = 1000
+mutation_probability = 0.2
+crossover_probability = 1
+selection_percentage = 0.6
+population_size = 800
 max_iterations = 100
-eps = 1e-4
+uppercase_probability = 0.4
+
+# Crayon setup
+cc = CrayonClient('localhost')
+cc.remove_all_experiments()
+exp = cc.create_experiment('Dataset Content Evolution - {}'.format(datetime.now()))
 
 
 def clean_text(text: str):
@@ -46,16 +57,10 @@ def character_distribution(text: str):
     return distribution
 
 
-def simplify_distribution(original_distribution: np.array):
-    ret = original_distribution[0:26]
-    return ret + original_distribution[26:52]
-
-
 def count_duplicates(text: str):
     words = text.split()
     words_unique = set(words)
     return len(words) - len(words_unique)
-
 
 
 """
@@ -63,111 +68,122 @@ def count_duplicates(text: str):
 """
 
 
-def generate_random_solution(bag_of_words: set):
-    words = sample(bag_of_words, solution_size)
-    unique_words = list(set(words))
-    restore_individual_size(unique_words, bag_of_words)
-    return ' '.join(unique_words)  # TODO: express size as characters number, not word count
+def sample_new_words(bag_of_words: set, number_of_words: int):
+    new_words = sample(bag_of_words, number_of_words)
+    for word in new_words:
+        if npchoice([True, False], p=[uppercase_probability, 1 - uppercase_probability]):
+            new_words.remove(word)
+            new_words.append(word.capitalize())
+    return new_words
 
 
-def restore_individual_size(ind: list, bag_of_words: set):
-    while len(ind) != solution_size:
-        additional_words = sample(bag_of_words, solution_size - len(ind))
-        ind.extend(additional_words)
-        ind = list(set(ind))
-    return ind
+def generate_random_solution(bag_of_words: set, mean_words_length: float):
+    words = sample_new_words(bag_of_words, int(np.ceil(solution_size/mean_words_length)))
+    restore_individual_size(words, bag_of_words, mean_words_length)
+    return ' '.join(words)
+
+
+def restore_individual_size(ind: list, bag_of_words: set, mean_words_length: float):
+    ind_str = ' '.join(ind)
+    ind_list = ind
+    num_missing_characters = solution_size - len(ind_str)
+    if num_missing_characters > 0:
+        ind_list.extend(sample_new_words(bag_of_words, int(np.ceil(num_missing_characters/mean_words_length))))
+        ind_list = list(set(ind_list))
+    return ind_list
+
+
+def mean_length(individuals: list):
+    lengths = [len(ind) for ind in individuals]
+    return np.mean(lengths)
 
 
 def evaluate_fitness(solution: str):
-    assert (len(solution.split()) == solution_size)
-    distr = simplify_distribution(character_distribution(solution))
-    if len(solution) > 0:
-        loss = np.sum(np.abs(distr - target_distribution))
-    else:
-        loss = 1
-    return 1/(loss + eps)
+    distr = character_distribution(solution)
+    loss = np.sum(np.abs(distr - target_distribution))
+    return 1 / (loss + 1)
 
 
-def single_mutation(sol: str, bag_of_words: set):
+def single_mutation(sol: str, bag_of_words: set, mean_words_length: float):
     l = sol.split()
-    l.remove(l[randint(0, solution_size)])
-    l = restore_individual_size(l, bag_of_words)
+    l.remove(l[randint(0, len(l))])
+    l = restore_individual_size(l, bag_of_words, mean_words_length)
     return ' '.join(l)
 
 
-def mutation(individuals: list, bag_of_words: set):
+def mutation(individuals: list, bag_of_words: set, mean_words_length: float):
     ret = []
     for ind in individuals:
         if npchoice([True, False], p=[mutation_probability, 1-mutation_probability]):
-            ret.append(single_mutation(ind, bag_of_words))
+            ret.append(single_mutation(ind, bag_of_words, mean_words_length))
         else:
             ret.append(ind)
     return ret
 
 
-def single_crossover(sol1: str, sol2: str, bag_of_words: set):
+def single_crossover(sol1: str, sol2: str, bag_of_words: set, mean_words_length: float):
     list1 = np.array(sol1.split())
     list2 = np.array(sol2.split())
-    shuffle(list1)
-    shuffle(list2)
-    child1idx = np.array(sample(list(range(solution_size)), int(solution_size/2)))
+    if len(list1) < len(list2):
+        child1idx = np.array(sample(list(range(len(list1))), int(len(list1) / 2)))
+    else:
+        child1idx = np.array(sample(list(range(len(list2))), int(len(list2) / 2)))
     tmp = list1[child1idx]
     list1[child1idx] = list2[child1idx]
     list2[child1idx] = tmp
-    child1_list = restore_individual_size(list(set(list1)), bag_of_words)
-    child2_list = restore_individual_size(list(set(list2)), bag_of_words)
-    assert len(child1_list) == solution_size and len(child2_list) == solution_size
+    child1_list = restore_individual_size(list(set(list1)), bag_of_words, mean_words_length)
+    child2_list = restore_individual_size(list(set(list2)), bag_of_words, mean_words_length)
     child1 = ' '.join(child1_list)
     child2 = ' '.join(child2_list)
     return child1, child2
 
 
-def crossover(parents: list, bag_of_words: set):
-    num_parents = len(parents)
+def crossover(parents: list, bag_of_words: set, mean_words_length: float):
     children = []
-    while len(children) < num_parents:
+    while len(children) < population_size:
         p1 = choice(parents)
         p2 = choice(parents)
-        if npchoice([True, False], p=[crossover_probability, 1-crossover_probability]):
-            c1, c2 = single_crossover(p1, p2, bag_of_words)
-        else:
-            c1 = p1
-            c2 = p2
+        c1, c2 = single_crossover(p1, p2, bag_of_words, mean_words_length)
         children.extend([c1, c2])
     return children
 
 
-def selection(individuals: list, scores: list, bag_of_words: set):
+def selection(individuals: list, scores: list):
     s = [sol.split() for sol in individuals]
     s = np.array([ind for _, ind in sorted(zip(scores, s), reverse=True)])
     ret = list(s[0:int(np.floor(len(s)*selection_percentage))])
-    new_elements = [np.array(generate_random_solution(bag_of_words).split()) for _ in range(len(s) - len(ret))]
-    ret.extend(new_elements)
     ret = [' '.join(set(i)) for i in ret]
     return ret
 
 
-def evolve(bag_of_words: set, verbose: bool = False):
-    individuals = [generate_random_solution(bag_of_words) for _ in range(population_size)]
+def evolve(bag_of_words: set, mean_words_length, verbose: bool = False, show_intermediate_results: bool = False):
+    individuals = [generate_random_solution(bag_of_words, mean_words_length) for _ in range(population_size)]
     for iter in range(max_iterations):
         do_print = False
-        if iter % 5 == 0:
+        if iter % 10 == 0:
             do_print = True
+        if show_intermediate_results and iter % 10 == 0:
+            show_best_result(individuals)
         if verbose:
             print('Iteration {}: Computing fitness...'.format(iter+1), end='\r')
         scores = [evaluate_fitness(ind) for ind in individuals]
+        exp.add_scalar_value("Evol/Min Fitness", np.min(scores))
+        exp.add_scalar_value("Evol/Max Fitness", np.max(scores))
+        exp.add_scalar_value("Evol/Mean Fitness", np.mean(scores))
+        exp.add_scalar_value("Evol/Mean Length of Individuals", mean_length(individuals))
         if do_print:
             print('Iteration {}: min fitness {}, max fitness {}, mean fitness {}'.format(iter+1, np.min(scores),
                                                                                          np.max(scores), np.mean(scores)))
         if verbose:
             print('Iteration {}: Selecting parents...'.format(iter + 1), end='\r')
-        parents = selection(individuals, scores, bag_of_words)
+        parents = selection(individuals, scores)
         if verbose:
             print('Iteration {}: Crossover...'.format(iter + 1), end='\r')
-        children = crossover(parents, bag_of_words)
+        children = parents
+        children.extend(crossover(parents, bag_of_words, mean_words_length))
         if verbose:
             print('Iteration {}: Mutating individuals...'.format(iter + 1), end='\r')
-        children = mutation(children, bag_of_words)
+        children = mutation(children, bag_of_words, mean_words_length)
         individuals = children
     return individuals
 
@@ -177,6 +193,8 @@ def show_best_result(results: list):
     best_result = np.array([ind for _, ind in sorted(zip(scores, results), reverse=False)])[0]
     dist = character_distribution(best_result)
     plt.plot(dist)
+    plt.plot(target_distribution)
+    plt.plot(original_text_distribution)
     plt.show()
 
 
@@ -186,14 +204,43 @@ def show_best_result(results: list):
 
 
 if __name__ == '__main__':
-    filepath = '../data/orig_wikipedia.txt'
-    with open(filepath, 'r') as f:
-        init = f.read()
-    text = delete_duplicates(init)
-    text = clean_text(text)
-    text = delete_duplicates(text)
-    bag_of_words = set(text.split())
-    res = evolve(bag_of_words, False)
+    # Adjust target distribution
+    target_distribution[0:26] = target_distribution[0:26] / 10  # Capital letters
+    target_distribution[52:68] = target_distribution[52:68] / 10  # Symbols and digits
+    target_distribution[69] = target_distribution[69] * 4  # Blank space
+    target_distribution = target_distribution / np.sum(target_distribution)
+
+    load_files = False
+    list_of_words = []
+    if load_files:
+        folder_path = '../data/text/'
+        print('Processing files...')
+        files = [f for f in listdir(folder_path) if isfile(join(folder_path, f))]
+        for file in files:
+            print('File ', file)
+            with open(join(folder_path, file)) as f:
+                init = f.read()
+            text = delete_duplicates(init)
+            text = clean_text(text)
+            text = delete_duplicates(text)
+            list_of_words.extend(text.split())
+        bag_of_words = set(list_of_words)
+        bag_of_words = set([s.lower() for s in list(bag_of_words)])
+        print('Files processed.')
+        with open('./bag_of_words.pkl', 'wb') as output:
+            pickle.dump(bag_of_words, output, pickle.HIGHEST_PROTOCOL)
+    else:
+        with open('./bag_of_words.pkl', 'rb') as input:
+            bag_of_words = pickle.load(input)
+    print('Total number of words: ', len(bag_of_words))
+    # Compute original text distribution
+    original_text_distribution = character_distribution(' '.join(list(bag_of_words)))
+
+    words_length = np.array([len(x) for x in bag_of_words])
+    mean_words_length = np.mean(words_length)
+    print('Mean length of a word: ', mean_words_length)
+    print('Start evolution')
+    res = evolve(bag_of_words, mean_words_length, True, True)
     results_filename = './dataset_content_res.pkl'
     with open(results_filename, 'wb') as output:
         pickle.dump(res, output, pickle.HIGHEST_PROTOCOL)
