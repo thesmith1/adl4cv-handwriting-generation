3e26d39db43caf6b0a1e168512cd65bc33ebd4f1
Paolo Notaro <paulstpr@gmail.com>
2018-12-14 18:23:05 +0100

    modified crop utility for big dataset

--- utils/crop_utility.py
+++ utils/crop_utility.py
@@ -1,25 +1,28 @@
 import cv2
 import numpy as np
 import os
-from sys import argv, exit
+from sys import exit
+import argparse
 
 FATAL = -1
 
+valid_style_ids = [0, 1]
 win_name = "Crop image"
-dest_save_folder = "../data/raw"
-init_rectangle_shape = (50, 50)
+dest_save_folder = "../data/big/raw"
+rectangle_shape = (90, 30)  # height must be tuned to match the height of two lines in a college-ruled notebook
 resizable = False
 rectangle_color = (0, 0, 0)
 DEBUG = False
 
 # keys
 QUIT_KEY = ord('q')
+INPUT_KEY = ord('i')
 LEFT_ARROW_KEY = 81
 UP_ARROW_KEY = 82
 RIGHT_ARROW_KEY = 83
 DOWN_ARROW_KEY = 84
 RETURN_KEY = 13
-KEY_SPEED = 5
+KEY_SPEED = 1
 
 
 class Rectangle:
@@ -35,12 +38,17 @@ class Rectangle:
 
 class WindowOperationHandler:
 
-    def __init__(self, window, image, init_rectangle, save_folder):
+    def __init__(self, window, image, init_rectangle, save_folder, text):
         self.window = window
         self.image = image
-        self.image_with_rect = np.copy(image)
+        self.image_drawable = np.copy(image)
         self.rectangle = init_rectangle
         self.save_folder = save_folder
+
+        self.text = text
+        self.previous_char = " "
+        self.char_annotations = dict()
+
         self.taken_filenames = set()
         self.current_filename_index = self.load_current_index()
 
@@ -56,14 +64,29 @@ class WindowOperationHandler:
         return current_index
 
     def save_crop(self):
+
+        # obtain crop
         x1, y1 = self.rectangle.x0, self.rectangle.y0
         x2, y2 = x1 + self.rectangle.w, y1 + self.rectangle.h
         cropped_image = self.image[y1:y2, x1:x2]
 
+        # obtain image filename
         self.current_filename_index += 1
         filename = "{}.jpg".format(self.current_filename_index)
+
+        # save image
         cv2.imwrite(os.path.join(self.save_folder, filename), cropped_image)
-        print("Crop saved as '%s' in '%s'." % (filename, self.save_folder))
+
+        # produce char annotation
+        current_char = self.text[0]
+        next_char = self.text[1]
+        print("Crop saved as '%s' in '%s'. "
+              "Char annotation: prev='%s', curr='%s', next='%s'." % (filename, self.save_folder, self.previous_char,
+                                                                     current_char, next_char))
+        self.text = self.text[1:]  # slide on text by one
+        char_annotation = map(lambda char: "_" if char == " " else char, (self.previous_char, current_char, next_char))
+        self.char_annotations[filename] = char_annotation
+        self.previous_char = current_char
 
     def mouse_callback(self, event, x, y, flags=None, param=None):
         if event == cv2.EVENT_LBUTTONDBLCLK:
@@ -71,7 +94,7 @@ class WindowOperationHandler:
                 self.save_crop()
                 x1, y1 = self.rectangle.x0, self.rectangle.y0
                 x2, y2 = self.rectangle.x0 + self.rectangle.w, self.rectangle.y0 + self.rectangle.h
-                cv2.rectangle(self.image, pt1=(x1, y1), pt2=(x2, y2), color=rectangle_color, thickness=-1)
+                cv2.rectangle(self.image_drawable, pt1=(x1, y1), pt2=(x2, y2), color=rectangle_color, thickness=-1)
                 if DEBUG:
                     print("Detected double click inside rectangle!")
             else:
@@ -90,7 +113,7 @@ class WindowOperationHandler:
                 self.rectangle.y0 = y - self.previous_coordinates[1] + self.previous_top_left_coordinates[1]
                 x1, y1 = self.rectangle.x0, self.rectangle.y0
                 x2, y2 = self.rectangle.x0 + self.rectangle.w, self.rectangle.y0 + self.rectangle.h
-                cv2.imshow("Zoom", self.image_with_rect[y1:y2, x1:x2])
+                cv2.imshow("Zoom", self.image[y1:y2, x1:x2])
             elif self.resizing:
                 self.rectangle.w = x - self.rectangle.x0
                 self.rectangle.h = y - self.rectangle.y0
@@ -116,15 +139,18 @@ class WindowOperationHandler:
         delta_x = 0
         delta_y = 0
         if key == UP_ARROW_KEY:
-            delta_y = -1*KEY_SPEED
+            delta_y = -1 * KEY_SPEED
         elif key == DOWN_ARROW_KEY:
-            delta_y = 1*KEY_SPEED
+            delta_y = 1 * KEY_SPEED
         elif key == LEFT_ARROW_KEY:
-            delta_x = -1*KEY_SPEED
+            delta_x = -1 * KEY_SPEED
         elif key == RIGHT_ARROW_KEY:
-            delta_x = 1*KEY_SPEED
+            delta_x = 1 * KEY_SPEED
         elif key == RETURN_KEY:
             self.save_crop()
+            x1, y1 = self.rectangle.x0, self.rectangle.y0
+            x2, y2 = x1 + self.rectangle.w, y1 + self.rectangle.h
+            cv2.rectangle(self.image_drawable, pt1=(x1, y1), pt2=(x2, y2), color=rectangle_color, thickness=-1)
 
         # apply delta
         if self.rectangle.x0 + delta_x >= 0 and self.rectangle.x0 + self.rectangle.w + delta_x < self.image.shape[1]:
@@ -134,45 +160,106 @@ class WindowOperationHandler:
 
         x1, y1 = self.rectangle.x0, self.rectangle.y0
         x2, y2 = x1 + self.rectangle.w, y1 + self.rectangle.h
-        cv2.imshow("Zoom", self.image_with_rect[y1:y2, x1:x2])
+        cv2.imshow("Zoom", self.image[y1:y2, x1:x2])
 
 
-def main_program_window(image):
-    h, w, _ = img.shape
+def save_label_file(label_file_path, annotations, style_id):
+    with open(label_file_path, "a") as lf:
+        for filename, char_annotation in annotations.items():
+            lf.write("{} {} {} {} {}\n".format(filename, *char_annotation, style_id))
 
+
+def main_program_window(image, text, args):
+    h, w, _ = image.shape
     cv2.namedWindow(win_name, cv2.WINDOW_FULLSCREEN)
     cv2.imshow(win_name, image)
 
-    h_rect, w_rect = init_rectangle_shape
+    h_rect, w_rect = rectangle_shape
     x0_rect, y0_rect = (w - w_rect) // 2, (h - h_rect) // 2
     rect = Rectangle(p0=(x0_rect, y0_rect), height=h_rect, width=w_rect)
-    handler = WindowOperationHandler(win_name, image, rect, dest_save_folder)
+    handler = WindowOperationHandler(win_name, image, rect, dest_save_folder, text)
     cv2.setMouseCallback(win_name, handler.mouse_callback)
 
     done = False
     while not done:
 
-        image_with_rect = np.copy(image)
-        cv2.rectangle(image_with_rect, pt1=(rect.x0, rect.y0), pt2=(rect.x0 + rect.w, rect.y0 + rect.h),
+        # show image and crop rectangle
+        image_drawable = np.copy(handler.image_drawable)
+        cv2.rectangle(image_drawable, pt1=(rect.x0, rect.y0), pt2=(rect.x0 + rect.w, rect.y0 + rect.h),
                       color=rectangle_color, thickness=2)
-        cv2.imshow(win_name, image_with_rect)
+        cv2.imshow(win_name, image_drawable)
+
+        # check for key presses
         key = cv2.waitKey(1)
-        if key == QUIT_KEY:
+        if key == QUIT_KEY:  # quitting procedure
+
+            valid = False
+            save_response = None
+            while not valid:
+                save_response = input("You want to save? [y/n]")
+                if save_response in ("y", "n"):
+                    valid = True
+                else:
+                    print("Invalid response.")
+            if save_response == "y":
+
+                # check style was provided
+                if args.style is None:
+                    valid = False
+                    while not valid:
+                        insertion = input("No style id was provided, but it is required in order to save. Style id:")
+                        try:
+                            style_id = int(insertion)
+                            if style_id not in valid_style_ids:
+                                print("Invalid style id.")
+                            else:
+                                args.style = style_id
+                                valid = True
+                        except ValueError:
+                            print("Invalid style id.")
+
+                # save label file
+                print("Saving label file...", end='')
+                save_label_file(args.output_label_file, handler.char_annotations, args.style)
+                print("done.")
+
             done = True
+
+        elif key == INPUT_KEY:
+            insertion = input('Append string in text buffer:')
+            text.extend(insertion)
         elif key != -1:
             handler.keyboard_callback(key)
 
 
-if __name__ == '__main__':
-    if len(argv) != 2:
-        print("Invalid input arguments. Usage: python crop_utility.py <image-path>")
-        exit(FATAL)
-
+def main(args):
     # open image
-    image_path = argv[1]
+    image_path = args.image_file
     img = cv2.imread(image_path)
     if img is None:
         print("Invalid path to image specified. '%s' not found" % image_path)
         exit(FATAL)
 
-    main_program_window(img)
+    # load text
+    with open(args.input_text_file, "r") as f:
+        text = f.read()
+
+    # check label file
+    assert os.path.isfile(args.output_label_file), "Invalid label file."
+
+    main_program_window(img, text, args)
+
+
+if __name__ == '__main__':
+    # arg parse
+    p = argparse.ArgumentParser(prog="python crop_utility.py", description="To produce annotated crops")
+    p.add_argument('input_text_file', help="Requires to specify an input text file for labeling",
+                   type=str)
+    p.add_argument('output_label_file', help="Requires to specify an output label file",
+                   type=str)
+    p.add_argument('image_file', help="Requires to specify the path of the image to crop", type=str)
+    p.add_argument('-s', '--style', help="Allows to specify the style id for the characters inside the image",
+                   type=int, default=None, choices=valid_style_ids)
+    arguments = p.parse_args()
+
+    main(arguments)
