2e195460596b06782a27f88f19abbc707d88a15c
Paolo Notaro <paulstpr@gmail.com>
2018-12-15 20:13:18 +0100

    adapted preprocessing to big dataset

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -3,55 +3,80 @@ import cv2
 import numpy as np
 from utils.image_utils import is_image
 
-base_dir = "../data"
+base_dir = "../data/big/"
 source_dir = os.path.join(base_dir, "raw")
 dest_dir = os.path.join(base_dir, "processed")
 
-WHITE = 255
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (125, 125, 125)
+BLACK_LINE_THRESHOLD = 200
+BLACK_LINE_LENGTH = 15
 BORDER_WIDTH = 5
 
 
-def process_image(image):
+def color_k_means(image, mu_init, channels_to_look=None):
 
-    # gray-scaling
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    if channels_to_look is None:
+        channels_to_look = [0, 1, 2]
 
-    # 2-means clustering white saturation
-    mu = [80, 160]
-    z = np.zeros_like(image, dtype=np.uint8)
-    z_previous = np.ones_like(image)
+    mu = mu_init
+    z = np.zeros(image.shape[:2], dtype=np.uint8)
+    z_previous = np.ones_like(z)
+
     while not np.all(z == z_previous):
 
         z_previous = np.copy(z)
         # E-step
         for i, row in enumerate(image):
-            for j, element in enumerate(row):
-                distances = [np.linalg.norm(element - mu_i) for mu_i in mu]
+            for j, pixel in enumerate(row):
+                distances = [np.linalg.norm(pixel[channels_to_look] - mu_i) for mu_i in mu]
                 z[i, j] = np.argmin(distances)
         # M-step
         for i in (0, 1):
             if np.sum(z == i) == 0:
-                mu[i] = 80 * (i + 1)
+                mu[i] = mu_init[i]
             else:
                 mu[i] = np.mean(image[z == i])
-    image[z == 1] = WHITE
+
+    return z
+
+
+def process_image(image):
+
+    # 2-means clustering to find background
+    mu = np.array([BLACK, WHITE])
+    z = color_k_means(image, mu)
+    cv2.imshow("original", image)
+    image[z == 1] = BLACK
+    cv2.imshow("clustered", image)
+
+    # detect line
+    h, w = image.shape[:2]
+    for i in range(h // 2 - 5, h // 2 + 5):
+        for j in range(w):
+            window = image[i-2:i+2, j]
+            distance = np.mean(image[i, j]) - np.mean(window)
+            if distance > 45:
+                image[i, j] = BLACK
+
+    image[image != BLACK] = 255
+    cv2.imshow("line removed", image), cv2.waitKey(0)
 
     # border removal
-    image[0:BORDER_WIDTH, :] = WHITE
-    image[-BORDER_WIDTH:, :] = WHITE
-    image[:, 0:BORDER_WIDTH] = WHITE
-    image[:, -BORDER_WIDTH:] = WHITE
+    image[:BORDER_WIDTH, :] = BLACK
+    image[-BORDER_WIDTH:, :] = BLACK
 
-    # inversion
-    image = 255 - image
+    # gray-scaling
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    return image
+    return gray_image
 
 
 if __name__ == '__main__':
 
     all_filenames = os.listdir(source_dir)
-    for file_count, filename in enumerate(all_filenames):
+    for file_count, filename in enumerate(sorted(all_filenames)):
         if is_image(filename):
             print("Processing '%s' [%3d/%3d]..." % (filename, file_count + 1, len(all_filenames)), end='\r')
             image_path = os.path.join(source_dir, filename)
