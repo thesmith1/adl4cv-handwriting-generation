7a706621333de9a6b8a0b9d654f0020603568fa3
Paolo Notaro <paulstpr@gmail.com>
2019-01-28 12:14:06 +0100

    modified wcGAN training from epochs to iterations

--- model/wcGAN.py
+++ model/wcGAN.py
@@ -8,7 +8,7 @@ from numpy.random import randn, choice
 from numpy import concatenate
 
 import torch
-from torch.optim import Optimizer, RMSprop
+from torch.optim import Optimizer, Adam
 from torch.utils.data import DataLoader
 from torchvision.transforms import Compose, ToTensor, Resize, ToPILImage
 
@@ -50,9 +50,8 @@ class WCGAN:
         else:
             self._current_datetime = datetime.datetime.now()
 
-    def train(self, n_epochs: int, next_letter_to_add: str):
+    def train(self, n_iterations: int, next_letter_to_add: str):
         current_char_index = character_to_index_mapping[next_letter_to_add]  # ' ' is already present
-        max_GPU_memory = 0
         print('Starting epochs, GPU memory in use '
               'before loading the inputs: {} MB'.format(torch.cuda.memory_allocated(torch.cuda.current_device()) / 1e6))
 
@@ -80,38 +79,41 @@ class WCGAN:
         D_traced = torch.jit.trace(self._D, (torch.randn(bs, 1, IMAGE_HEIGHT, IMAGE_WIDTH).to(self._device),
                                              torch.randn(bs, NUM_CHARS * 3 + 1).to(self._device)))
 
-        # Epoch iteration
-        step = 0
-        for epoch in range(1, n_epochs + 1):
+        # Train iteration
+        for iteration in range(1, n_iterations + 1):
 
-            if epoch % add_character_every == 0 and current_char_index < NUM_CHARS:
+            labels, style = None, None
+            if iteration % add_character_every == 0 and current_char_index < NUM_CHARS:
                 self._dataset.add_character_to_training(list(character_to_index_mapping.keys())[current_char_index])
                 current_char_index = current_char_index + 1
+
+            start_time = time.time()
 
             self._G.train()
             self._D.train()
 
-            # Iterate over the dataset
-            start_time = time.time()
-            for batch_count, (X, labels, style) in enumerate(self._dataset_loader):
+            # ---------------------TRAIN D------------------------
+            D_loss = D_real = D_fake = None
+            D_losses = []
+            for i in range(1, eta_critic + 1):
+
+                # sample batch
+                X, labels, style = next(iter(self._dataset_loader))
 
                 # batch norm does not allow bs=1, so we check
                 if len(X) == 1:
                     continue
-                else:
-                    step += 1
 
                 # Arrange data
                 X = X.to(device=self._device)
                 char_conditioning = character_to_one_hot(labels)
                 c = concatenate([char_conditioning, style.unsqueeze(-1)], axis=1)
                 c = torch.from_numpy(c).to(device=self._device)
-                G_loss = None
 
                 # Reset gradient
                 self._D_optim.zero_grad()
 
-                # Sample data
+                # Sample z
                 z = torch.from_numpy(randn(len(X), NOISE_LENGTH)).to(device=self._device)
 
                 # Discriminator forward-loss-backward-update
@@ -119,57 +121,51 @@ class WCGAN:
                 D_real = D_traced(X, c)
                 D_fake = D_traced(G_sample.detach(), c)
                 D_loss = - (torch.mean(D_real) - torch.mean(D_fake))
+                D_losses.append(D_loss.item())
 
-                if D_loss > D_loss_threshold:
-                    D_loss.backward()
-                    self._D_optim.step(None)
-                    for param in self._D.parameters():
-                        param.data.clamp_(-clamp_limit, clamp_limit)
+                D_loss.backward()
+                self._D_optim.step(None)
+                for param in self._D.parameters():
+                    param.data.clamp_(-clamp_limit, clamp_limit)
 
-                self._writer.add_scalar("Loss/Discriminator", D_loss.item(), step)
-                self._writer.add_scalar("Discriminator response/to real images (average)", D_real.mean().item(),
-                                        step)
-                self._writer.add_scalar("Discriminator response/to fake images (average)", D_fake.mean().item(),
-                                        step)
+            print('D Iteration {:5d}, D losses {}'.format(iteration, D_losses))
 
-                if batch_count % eta_critic == 0:
-                    # Reset gradient
-                    self._G_optim.zero_grad()
+            self._writer.add_scalar("Loss/Discriminator", D_loss.item(), iteration)
+            self._writer.add_scalar("Critic response/to real images (average)", D_real.mean().item(), iteration)
+            self._writer.add_scalar("Critic response/to fake images (average)", D_fake.mean().item(), iteration)
 
-                    # Generator forward-loss-backward-update
+            # ---------------------TRAIN G------------------------
+            self._G_optim.zero_grad()
 
-                    z = torch.from_numpy(randn(len(X), NOISE_LENGTH)).to(self._device)
-                    G_sample = G_traced(z, c)
-                    D_fake = D_traced(G_sample, c)
-                    G_loss = - torch.mean(D_fake)
-                    if G_loss > G_loss_threshold:
-                        G_loss.backward()
-                        self._G_optim.step(None)
+            # prepare G inputs
+            char_conditioning = character_to_one_hot(labels)
+            c = concatenate([char_conditioning, style.unsqueeze(-1)], axis=1)
+            c = torch.from_numpy(c).to(device=self._device)
 
-                    self._writer.add_scalar("Loss/Generator", G_loss.item(), step)
+            # Generator forward-loss-backward-update
+            z = torch.from_numpy(randn(len(char_conditioning), NOISE_LENGTH)).to(self._device)
+            G_sample = G_traced(z, c)
+            D_fake = D_traced(G_sample, c)
+            G_loss = - torch.mean(D_fake)
+            G_loss.backward()
+            self._G_optim.step(None)
 
-                # Store max allocated GPU memory
-                max_GPU_memory = max(max_GPU_memory, torch.cuda.max_memory_allocated(torch.cuda.current_device()) / 1e6)
+            self._writer.add_scalar("Loss/Generator", G_loss.item(), iteration)
 
-                last_char_added = next((char for char, index in character_to_index_mapping.items()
-                                        if index == current_char_index - 1), None)
-                print('Epoch {:4d}, batch {:3d}/{:3d}, D loss {}, G loss {}, '
-                      'max GPU memory allocated {:.2f} MB, last char added: {}'.format(epoch, batch_count + 1,
-                                                                                       len(self._dataset_loader),
-                                                                                       D_loss, G_loss, max_GPU_memory,
-                                                                                       last_char_added), end='\r')
-
+            last_char_added = next((char for char, index in character_to_index_mapping.items()
+                                    if index == current_char_index - 1), None)
             end_time = time.time()
-            print('\nEpoch completed in {:.2f} s'.format(end_time - start_time))
-
-            if epoch % save_every == 0:
+            print('G Iteration {:5d}, G loss {:4f}, time/iteration: {:3f}s, '
+                  'last char added: {}'.format(iteration, G_loss, end_time - start_time, last_char_added))
+            # save
+            if iteration % save_every == 0:
                 print("Saving...", end='')
-                torch.save(self._G, "../data/models/W_G_{}.pt".format(str(self._current_datetime)))
-                torch.save(self._D, "../data/models/W_D_{}.pt".format(str(self._current_datetime)))
+                torch.save(self._G, "../data/models/G_W_{}.pt".format(str(self._current_datetime)))
+                torch.save(self._D, "../data/models/D_W_{}.pt".format(str(self._current_datetime)))
                 print("done.")
 
             # produce graphical results
-            if epoch % produce_every == 0:
+            if iteration % produce_every == 0:
 
                 print("Producing evaluation results...", end='')
                 self._G.eval()
@@ -178,10 +174,10 @@ class WCGAN:
                 images = G_traced(fixed_latent_points, fixed_conditioning_inputs)
                 for image, letter in zip(images[:NUM_CHARS], letters_to_watch):
                     image = finalizing_transform(image.cpu().detach())
-                    self._writer.add_image("Fixed latent points/" + letter + "_P", image, global_step=epoch)
+                    self._writer.add_image("Fixed latent points/" + letter + "_P", image, global_step=iteration)
                 for image, letter in zip(images[NUM_CHARS:], letters_to_watch):
                     image = finalizing_transform(image.cpu().detach())
-                    self._writer.add_image("Fixed latent points/" + letter + "_G", image, global_step=epoch)
+                    self._writer.add_image("Fixed latent points/" + letter + "_G", image, global_step=iteration)
 
                 # generate random character images
                 random_characters_to_generate = list(key for key, value in character_to_index_mapping.items()
@@ -194,7 +190,7 @@ class WCGAN:
                         image = finalizing_transform(image.unsqueeze(0))
                         fig = produce_figure(image, "prev: {}, curr: {}, "
                                                     "next: {}, style: {}".format(*character_conditioning, style))
-                        self._writer.add_figure("Random characters/%d" % i, fig, global_step=epoch)
+                        self._writer.add_figure("Random characters/%d" % i, fig, global_step=iteration)
 
                 print("done.")
 
@@ -249,8 +245,8 @@ if __name__ == '__main__':
 
     # Init optimizers
     print('Initializing the optimizers...')
-    g_adam = RMSprop(g.parameters(), lr=5e-5, weight_decay=dis_l2_reg)
-    d_adam = RMSprop(d.parameters(), lr=5e-5, weight_decay=gen_l2_reg)
+    g_adam = Adam(g.parameters(), lr=1e-4, weight_decay=dis_l2_reg)
+    d_adam = Adam(d.parameters(), lr=1e-4, weight_decay=gen_l2_reg)
 
     # Load the dataset
     print("Loading dataset...")
