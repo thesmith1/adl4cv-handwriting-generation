small fixes and readability enhancements bug fix: the average was computed erroneously

stitching/stitching.py
@@ -6,14 +6,14 @@ from os.path import join, isfile
 
 import numpy as np
 import torch
-from matplotlib.pyplot import imshow, show, figure, plot
+from matplotlib.pyplot import imshow, show, figure
 from torchvision.transforms import Compose, ToTensor, Resize, ToPILImage
 
 lib_path = os.path.abspath(os.path.join(__file__, '..'))
 sys.path.append(lib_path)
 
 from utils.global_vars import IMAGE_WIDTH, rectangle_shape, SUP_REMOVE_WIDTH, INF_REMOVE_WIDTH
-from utils.image_utils import generate, generate_optimized, MEAN_OF_THREE, CONTRAST_INCREASE
+from utils.image_utils import generate_optimized, CONTRAST_INCREASE
 
 height_dim = 0
 width_dim = 1
@@ -22,33 +22,44 @@ BLACK_CORRELATION_OFFSET = 0.3
 WINDOW_RAMP_PROPORTION = 0.25
 
 
-# Accepts ndarrays or torch.Tensor of ndims=2
+
 def stitch(t1, t2):
+    """
+    Accepts ndarrays or torch.Tensor of ndims=2
+    :param t1: ndarray|Tensor with number of dimensions = 2
+    :param t2: ndarray|Tensor with number of dimensions = 2
+    :return: ndarray containing t1, t2 stitched
+    """
+    # re-cast
     if type(t1) == torch.Tensor:
         t1 = t1.numpy()
     if type(t2) == torch.Tensor:
         t2 = t2.numpy()
+
     # Offset to include correlation on the black pixels
     t1 = t1 - BLACK_CORRELATION_OFFSET
     t2 = t2 - BLACK_CORRELATION_OFFSET
     overlap_limit = int(t2.shape[width_dim] * OVERLAP_LIMIT_THRESHOLD)
     ramp_length = int((overlap_limit - 1) * WINDOW_RAMP_PROPORTION)
-    corr_vals = []
+
+    # compute correlation
+    corr_values = []
     window = np.ones((overlap_limit - 1, ))
     window[:ramp_length] = np.linspace(0.3, 1, ramp_length)
     window[-ramp_length:] = np.linspace(1, 0.3, ramp_length)
     for overlap_idx in range(1, overlap_limit):
-        normalization_coeff = t1[:, -overlap_idx:].shape[0] * t1[:, -overlap_idx:].shape[1]
-        corr = np.sum(np.sum((t1[:, -overlap_idx:] * t2[:, :overlap_idx]))) / normalization_coeff
-        corr_vals.append(corr)
-    best_overlap_idx = np.argmax(corr_vals * window) + 1
-    # Correct offset
+        normalization_coefficient = t1[:, -overlap_idx:].shape[height_dim] * t1[:, -overlap_idx:].shape[width_dim]
+        corr = np.sum(np.sum((t1[:, -overlap_idx:] * t2[:, :overlap_idx]))) / normalization_coefficient
+        corr_values.append(corr)
+    best_overlap_idx = np.argmax(corr_values * window) + 1
+
+    # correct offset
     t1 = t1 + BLACK_CORRELATION_OFFSET
     t2 = t2 + BLACK_CORRELATION_OFFSET
     left = t1[:, :-best_overlap_idx]
-    common_area = t1[:, -best_overlap_idx:] + t2[:, :best_overlap_idx] / 2
+    common_area = (t1[:, -best_overlap_idx:] + t2[:, :best_overlap_idx]) / 2
     right = t2[:, best_overlap_idx:]
-    return np.concatenate([left, common_area, right], axis=1), corr_vals, corr_vals * window
+    return np.concatenate([left, common_area, right], axis=1), corr_values, corr_values * window
 
 
 if __name__ == '__main__':
@@ -112,8 +123,8 @@ if __name__ == '__main__':
         if text == 'quit':
             break
         text = ' ' + text + ' '
-        characters = [generate_optimized(g, (text[i], text[i + 1], text[i + 2]), style, CONTRAST_INCREASE, dev) for i in
-                      range(len(text) - 2)]
+        characters = [generate_optimized(g, (text[i], text[i + 1], text[i + 2]), style, CONTRAST_INCREASE, dev)
+                      for i in range(len(text) - 2)]
         total = characters[0]
         for i in range(len(characters) - 1):
             total, _, _ = stitch(total, characters[i + 1])