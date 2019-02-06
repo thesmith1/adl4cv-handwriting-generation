7ee24971257e0d9df125c6949543aa0e33f6c8ae
Giorgio Fabbro <ga53kug@mytum.de>
2019-02-06 16:29:47 +0100

    Finally fixed setup_frontend.sh.

--- webapp/front/setup_frontend.sh
+++ webapp/front/setup_frontend.sh
@@ -2,6 +2,4 @@
 # Frontend
 curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
 sudo apt-get install -y nodejs
-sudo npm install @angular/cli --save-dev
-sudo npm install --save @angular/material @angular/cdk @angular/animations
-sudo npm install --save-dev @angular-devkit/build-angular
+sudo npm install
